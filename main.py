63b7a1e997960d25ae8ca2c4f60c12194184f95c
DinitoThompson <dinitothompson@gmail.com>
2023-04-16 16:06:46 -0500

    feat: Implement Save LP Feature

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 # Import files
 from Utility.detector import Detector
 
+# Sam Semegment Anything Model
+# Microsoft Azure - Cognative Services
 
 # Read different file type
-videoPath = ""
-imagePath = "./Data/Images/JA_Vehicles (2).jpg"
-webcamPath = 0
+imagePath = "./Data/Images/Vehicle (1).png"
+# webcamPath = 0
 
 # Initialize Detector Class
 detector = Detector()
 
 # Read image
 detector.LP_Image_Detection(imagePath)
-
-# Read video
-# detector.LP_Video_Detection_2(webcamPath)
-# detector.LP_Video_Detection_3(videoPath)
