25d47023ea2320337f219662d8c064427c4447a6
Nick9299 <nicholaityler@gmail.com>
2023-04-17 16:56:31 -0500

    chore: Updated vehicle search functions

--- Utility/video_search.py
+++ Utility/video_search.py
@@ -1,37 +1,42 @@
-path = "test.txt"  # text file path
+import os
+path = "" #insert path to saved folder here
+
+class vehicleSearch:
+    def licenseSearch(license): #accepts a plate number
+        list = []
+        for file in os.listdir(path): #iterate through all files in the specified path
+            if (license in file): #if the plate is found in the file name it is added to a list
+                list.append(file.replace(".png", ""))
+
+        if not (list):
+            print("License plate:", license, "not found")
+        else:
+            for file in list:
+                print(file)
 
 
-def licenseSearch(license):
-    file = open(path, "r")
-    list = []
-    for x in file:
-        if (license in x):
-            list.append(x.replace("\n", ""))
-    file.close
+    def dateSearch(startDate, endDate):  
+        list = []
+        flag = False
+        for file in os.listdir(path):
+            if (startDate in file): #sets flag to true if start date is found
+                flag = True
+        
+            if(endDate in file): #sets flag back to false if end date is found and add files at end date to list
+                flag = False 
+                list.append(file.replace(".png", ""))
 
-    if not (list):
-        print("License plate:", license, "not found")
-    else:
-        for x in list:
-            print(x)
+            if (flag == True):
+                list.append(file.replace(".png", ""))
 
-
-def dateSearch(date):  # not sure if I should include time with the date
-    file = open(path, "r")
-    list = []
-    for x in file:
-        if (date in x):
-            list.append(x.replace("\n", ""))
-    file.close
-
-    if not (list):
-        print("License plates not found on:", date)
-    else:
-        for x in list:
-            print(x)
+        if not (list):
+            print("License plates not found between:", startDate, "and", endDate)
+        else:
+            for file in list:
+                print(file)
 
 
-# licenseSearch("PGJA34")
-# licenseSearch("111111")
-# dateSearch("1/2/2023")
-# dateSearch("1/4/2023")
+#vehicleSearch.licenseSearch("PGJA34")
+#vehicleSearch.licenseSearch("111111")
+vehicleSearch.dateSearch("4-16-2023","4-17-2023")
+#vehicleSearch.dateSearch("1/4/2023")
