5c41a12ac003bd99aa3d87405b1c1df1349c6093
DinitoThompson <dinitothompson@gmail.com>
2023-04-25 11:37:01 -0500

    chore: Update DateTimeRange

--- Utility/video_search.py
+++ Utility/video_search.py
@@ -1,5 +1,5 @@
 import os
-from datetimerange import DateTimeRange #pip install DateTimeRange
+from datetimerange import DateTimeRange  # pip install DateTimeRange
 path = "Data\Saved_Plates"  # insert path to saved folder here
 
 
@@ -23,11 +23,11 @@ class vehicleSearch:
         list = []
 
         for file in os.listdir(path):
-            range = DateTimeRange(startDate, endDate)#Creates the date range
-            range.start_time_format = "%m-%d-%Y"#Sets the date format to mm-dd-yyyy
+            range = DateTimeRange(startDate, endDate)  # Creates the date range
+            range.start_time_format = "%m-%d-%Y"  # Sets the date format to mm-dd-yyyy
             range.end_time_format = "%m-%d-%Y"
-            if(file[0:10] in range):#if the date section of the file name is in range
-                list.append(file)#add to list
+            if (file[0:10] in range):  # if the date section of the file name is in range
+                list.append(file)  # add to list
 
         if not (list):
             print("License plates not found between:",
@@ -41,5 +41,5 @@ class vehicleSearch:
 
 # vehicleSearch.licenseSearch("PGJA34")
 # vehicleSearch.licenseSearch("111111")
-#vehicleSearch.dateSearch("4-16-2023", "4-17-2023")
+# vehicleSearch.dateSearch("4-16-2023", "4-17-2023")
 # vehicleSearch.dateSearch("1/4/2023")
